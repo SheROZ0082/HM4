8ccc4777d5a23efcb3b4cb43c2f8c50b56fe1832
SheROZ0082 <112874487+SheROZ0082@users.noreply.github.com>
2022-10-30 11:52:38 +0600

    ыва

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,36 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
         createTwoListsCombineThemAndSortElementsByLength();
     }
+
     public static void createTwoListsCombineThemAndSortElementsByLength() {
         final ArrayList<String> a = new ArrayList<>();
-        final ArrayList<String> b = new ArrayList<>();
-        final ArrayList<String> c = new ArrayList<>();
         a.add("clown");
         a.add("boy");
         a.add("consequence");
         a.add("therefore");
         a.add("think");
         System.out.println(a);
+        ArrayList<String> b = new ArrayList<>();
         b.add("nwolc");
         b.add("yob");
         b.add("ecneuqesnoc");
         b.add("erofereht");
         b.add("kniht");
+        final ArrayList<String> c = new ArrayList<>();
+        Collections.reverse(b);
 
-        for (String value : a) {
-            c.add(value);
+        for (int i = 0; i < a.size(); i++) {
+            c.add(a.get(i));
+            c.add(b.get(i));
         }
-        for (String s : b) {
-            c.add(s);
-        }
+
         System.out.println(c);
         c.sort(Comparator.comparingInt(String::length));
         System.out.println(c);
